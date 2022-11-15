645f8d209658dca480fe3c6753bb8c65ca24f57d
jokijun <bini72aa@naver.com>
2022-11-15 16:59:56 +0900

    AJAX 비동기 서버 통신 리스트 출력

--- SpringMVC/src/main/java/com/spring/controller/BoardController.java
+++ SpringMVC/src/main/java/com/spring/controller/BoardController.java
@@ -1,6 +1,5 @@
 package com.spring.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,55 +10,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.spring.entity.Board;
 import com.spring.mapper.BoardMapper;
 
 @Controller
 public class BoardController {
-
-	@Autowired
-	private BoardMapper mapper;
 	
+	@Autowired
+	BoardMapper boardMapper;
+
+	@RequestMapping("/")
+	public String main() {
+		return "main";
+	}
+	//@ResponseBody - > jackson-databind (객체를 JSON 데이터포맷으로 변환)
 	@RequestMapping("/boardList.do")
-	public String boardList(Model model) {
-		
-		List<Board> list = mapper.getLists(); 
-		model.addAttribute("list", list);
-		return "boardList";
-	}
-	@GetMapping("/boardForm.do")
-	public String boardForm() {
-		return "boardForm";
-	}
-	@PostMapping("/boardInsert.do")
-	public String boardInsert(Board vo) { // title,content, writer 파라메터수집(Board)
-		mapper.boardInsert(vo); // 등록
-		
-		return "redirect:/boardList.do";
-	}
-	@GetMapping("/boardContent.do")
-	public String boardContent(int idx, Model model ) { // ?idx=6
-		Board vo = mapper.boardContent(idx);
-		//조회수 증가
-		mapper.boardCount(idx);
-		model.addAttribute("vo", vo); // #{vo.idx}.. 
-		return "boardContent";
-	}
-	@GetMapping("boardDelete.do/{idx}")
-	public String boardDelete(@PathVariable("idx") int idx) { // ?idx=6
-		mapper.boardDelete(idx); // 삭제
-		return "redirect:/boardList.do";
-	}
-	@GetMapping("/boardUpdateForm.do/{idx}")
-	public String boardUpdateForm(@PathVariable("idx") int idx, Model model) {
-		Board vo=mapper.boardContent(idx);
-		model.addAttribute("vo", vo);		
-		return "boardUpdate"; // boardUpdate.jsp
-	}
-	@PostMapping("/boardUpdate.do")
-	public String boardUpdate(Board vo) { // idx, title, content
-		mapper.boardUpdate(vo); // 수정		
-		return "redirect:/boardList.do";
+	public @ResponseBody List<Board> boardList(){
+		List<Board> list = boardMapper.getLists();
+		return list; // JSON 데이터 형식으로 변환(API) 후 리턴(응답)
 	}
 }
